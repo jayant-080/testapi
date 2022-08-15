a9b33770a1b0542605504982398dea04411b3887
jayant-080 <guptajayant080@gmail.com>
2022-08-15 10:53:49 +0530

    changes

--- server.js
+++ server.js
@@ -7,10 +7,15 @@ app.use(express.json())
 
 app.get('/hi',(req,res)=>{
     res.status(200).json({
+        message:"hi boy"
+    })
+
+
+app.get('/hello',(req,res)=>{
+    res.status(200).json({
         message:"hello boy"
     })
 })
-
 
 
 app.listen(2000,()=>{
