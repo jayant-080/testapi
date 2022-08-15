fad64460331dd0da7cc2818b084cccdb94b91f98
jayant-080 <guptajayant080@gmail.com>
2022-08-15 10:42:37 +0530

    intial

---
+++ server.js
@@ -0,0 +1,18 @@
+import  express from "express"
+
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/hi',(req,res)=>{
+    res.status(200).json({
+        message:"hello boy"
+    })
+})
+
+
+
+app.listen(2000,()=>{
+    console.log("sercer is running on port 2000")
+})
